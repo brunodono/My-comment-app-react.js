ebeb56e06fc01c21f62f1cd890da85b71586130e
brunodono <bruno_bignard@hotmail.com>
2022-06-25 12:38:10 +0100

    Project and comment component created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Comment from './components/Comment';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My comments app</h1>
+      <Comment />
+      <Comment />
+      <Comment />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import "./Comment.css"
+
+const Comment = () => {
+    return (
+
+        <div className="comment">
+            <h2>Bruno Bignardi</h2>
+            <p>Esta dando certo</p>
+        </div>
+    );
+};
+
+export default Comment;
